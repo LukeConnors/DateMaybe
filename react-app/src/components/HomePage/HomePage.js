6ca06f15c7bb1feca0f4a803f3b3104222190a08
LukeConnors <luketconnors@yahoo.com>
2024-05-18 12:07:32 -0500

    started homepage component

---
+++ react-app/src/components/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from "react"
+
+
+function HomePage(){
+
+
+    return(
+        <>
+        <div>
+            <h1>Welcome to Date Maybe</h1>
+        </div>
+        </>
+    )
+}
+
+export default HomePage
