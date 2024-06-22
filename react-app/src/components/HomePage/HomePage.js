1299d5659334ad0c64532ba3c5e77159a557543e
LukeConnors <luketconnors@yahoo.com>
2024-06-21 23:40:07 -0500

    more home page work

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -10,7 +10,8 @@ function HomePage(){
             <h1>Welcome to Date Maybe</h1>
             <h3>What is Date Maybe?</h3>
             <p>Date Maybe is a dating app that has the unique feature of accepting incentive payments. On the premium plan of Date Maybe user's have
-            the opportunity to be matched together and sent on a date that is entirely paid for by their subscription payments!
+            the opportunity to be matched together and sent on a date that is entirely paid for by their subscription payments! Date Maybe's matchmakers
+            will find who is most compatible for you and send you on a tailor made first date!
             </p>
             <ul>
                 <li>
