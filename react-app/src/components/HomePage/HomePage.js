5ba114a0b15083d4db30d5da60876f8b728501f9
LukeConnors <luketconnors@yahoo.com>
2024-06-29 13:29:47 -0500

    adjusting html on home page

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -1,32 +1,32 @@
-import React, {useState, useEffect} from "react"
-import './HomePage.css'
+import React, { useState, useEffect } from "react";
+import "./HomePage.css";
 
-function HomePage(){
-
-
-    return(
-        <>
-        <div className="home-page-container">
-            <h1>Welcome to Date Maybe</h1>
-            <h3>What is Date Maybe?</h3>
-            <p>Date Maybe is a dating app that has the unique feature of accepting incentive payments. On the premium plan of Date Maybe user's have
-            the opportunity to be matched together and sent on a date that is entirely paid for by their subscription payments! Date Maybe's matchmakers
-            will find who is most compatible for you and send you on a tailor made first date!
-            </p>
-            <ul>
-                <li>
-
-                </li>
-                <li>
-
-                </li>
-                <li>
-                    
-                </li>
-            </ul>
+function HomePage() {
+  return (
+    <>
+      <div className="home-page-container">
+        <div>
+          <h1>Welcome to Date Maybe</h1>
+          <h3>What is Date Maybe?</h3>
+          <p>
+            Date Maybe is a dating app that has the unique feature of accepting
+            incentive payments. On the premium plan of Date Maybe user's have
+            the opportunity to be matched together and sent on a date that is
+            entirely paid for by their subscription payments! Date Maybe's
+            matchmakers will find who is most compatible for you and send you on
+            a tailor made first date!
+          </p>
         </div>
-        </>
-    )
+        <div>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
