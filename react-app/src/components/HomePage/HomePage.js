2bdbe4458c2d1e252fb6a284faf55afdad0ff1d2
LukeConnors <luketconnors@yahoo.com>
2024-09-07 19:37:37 -0500

    html

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -13,7 +13,7 @@ function HomePage() {
             incentive payments. On the premium plan of Date Maybe user's have
             the opportunity to be matched together and sent on a date that is
             entirely paid for by their subscription payments! Date Maybe's
-            matchmakers will find who is most compatible for you and send you on
+            matchmakers will find who is most compatible for you, and send you on
             a tailor-made first date.
           </p>
         </div>
